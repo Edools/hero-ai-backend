117aded9f101c820ef64667460835115f234afb3
Yoji Kojio <yoji.kojio@gmail.com>
2023-03-23 01:04:56 -0300

    :wrench: Allow cors from production domain

--- api/hero-ai.js
+++ api/hero-ai.js
@@ -7,8 +7,8 @@ const openai = new OpenAIApi(configration);
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader('Access-Control-Allow-Origin', 'https://hero-ai-front.vercel.app/')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
